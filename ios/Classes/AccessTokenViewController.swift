0f5a2afccb028f3844f04f46813879d1ab7ee872
AhmedOsman07 <88888259+AhmedOsman07@users.noreply.github.com>
2022-06-28 12:57:17 +0200

    Update AccessTokenViewController.swift

--- ios/Classes/AccessTokenViewController.swift
+++ ios/Classes/AccessTokenViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import WebKit
 import Combine
-
+@available(iOS 13.0, *)
 class AccessTokenViewController: UIViewController, WKUIDelegate, WKNavigationDelegate{
     
     var webView: WKWebView!
